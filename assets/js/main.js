981c0c1d94d70722c964c9ee5d3e0e377fc5fcd2
AbraheemK <62955482+Abraheem8k@users.noreply.github.com>
2020-06-09 01:31:11 +0300

    Revert "Adding if fucntion"
    This reverts commit 1700455116e69d0d8aa3e4a317f3881f0949e92c.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
  
-if ( ! sessionStorage.getItem( 'doNotShow' ) ) {
-	sessionStorage.setItem( 'doNotShow', true );
-	
-		$(window).on('load', function() { // makes sure the whole site is loaded 
-		$('#status').delay(4000).fadeOut(); // will first fade out the loading animation 
-		$('#preloader').delay(1500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-		$('body').delay(250).css({'overflow':'visible'});
-		})
-		else {
-			$ ('#status, #preloader').hide();
-		}
-}
+$(window).on('load', function() { // makes sure the whole site is loaded 
+	$('#status').delay(4000).fadeOut(); // will first fade out the loading animation 
+	$('#preloader').delay(1500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	$('body').delay(250).css({'overflow':'visible'});
+  })
 
 (function($) {
 
