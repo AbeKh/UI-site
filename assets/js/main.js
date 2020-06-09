baf2404ece62f6dbaad967d635a5a71747b417ad
AbraheemK <62955482+Abraheem8k@users.noreply.github.com>
2020-06-10 01:58:23 +0300

    adidtion of sessionStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,17 @@
 	$(function() {
 
 
+		if (sessionStorage.getItem('loader') !== 'true') {
+			$('#status').hide();
+			$('preloader').hide();
+		}
+
+
 		$(window).on('load', function() { // makes sure the whole site is loaded 
 			$('#status').delay(4000).fadeOut(); // will first fade out the loading animation 
 			$('#preloader').delay(1500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
 			$('body').delay(250).css({'overflow':'visible'});
+			sessionStorage.getItem('loader' , 'true');
 		  })
 
 		var	$window = $(window),
